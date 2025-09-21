296fe8d57822c2bda61a438a29f6ad2c495db9b3
mlcarvalho <mscarvlho@protonmail.com>
2025-09-20 21:53:56 -0300

    Aprimora controles

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
 const inputResultado = document.getElementById("res");
 const botaoGerar = document.getElementById("gerarSenha");
 const botaoCopiar = document.getElementById("copiar");
-const letraMinuscula = "abcdefghijklmnopqrstuvwxyz"
-
-let min = letraMinuscula.length;
+const tamanhoSenha = document.getElementById("tamanhoSenha");
+const letraMinuscula = "abcdefghijklmnopqrstuvwxyz";
+const letraMaiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const caracteresEspeciais = "!@#$%^&*()-_=+[]{};:'\"\\|,<.>/?`~";
+const numeros = "0123456789";
 
 botaoGerar.addEventListener("click", criaSenha);
 botaoCopiar.addEventListener("click", adicionaAoClipboard)
 
 
 function criaSenha() {
-for (let i = 0; i < min; i++){
-inputResultado.value += letraMinuscula[i];
+
+inputResultado.value = '';
+
+for (let i = 0; i < Number(tamanhoSenha.value); i++){
+
+inputResultado.value += letraMinuscula[
+    Math.floor(Math.random() * letraMinuscula.length)
+];
 }
 }
 
