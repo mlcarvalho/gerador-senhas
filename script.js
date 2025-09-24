8919eb6a496b43fc885141635732762b3de219b9
mlcarvalho <mscarvlho@protonmail.com>
2025-09-24 20:09:04 -0300

    Adiciona histórico com localStorage
    - Salva senhas geradas
    - Deleta Histórico
    - Alterna páginas entre gerador e histórico

--- script.js
+++ script.js
@@ -3,6 +3,11 @@ const inputResultado = document.getElementById("res");
 const botaoGerar = document.getElementById("gerarSenha");
 const botaoCopiar = document.getElementById("copiar");
 const tamanhoSenha = document.getElementById("tamanhoSenha");
+const historicoDiv = document.getElementById("historicoDiv");
+const btnfechar = document.getElementById("fechar");
+const btnDeletar = document.getElementById("deletar");
+const btnHistorico = document.getElementById("btnHistorico");
+const histAparece = document.getElementById("histAparece");
 
 //checkbox
 const caracteresEspeciais = document.getElementById("especiais");
@@ -16,9 +21,14 @@ const letraMaiuscula = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const simbolos = "!@#$%^&*()-_=+[]{};:'\"\\|,<.>/?`~";
 const numeros = "0123456789";
 
+let geradorSenhaDisplay = document.getElementById("geradorSenha");
+
 //Escuta os botões.
 botaoGerar.addEventListener("click", criaSenha);
-botaoCopiar.addEventListener("click", adicionaAoClipboard)
+botaoCopiar.addEventListener("click", adicionaAoClipboard);
+btnfechar.addEventListener("click", fecharHistorico);
+btnDeletar.addEventListener("click", deletaHistorico);
+btnHistorico.addEventListener("click", abreHist);
 
 //Função para gerar a senha
 function criaSenha() {
@@ -50,6 +60,41 @@ for (let i = 0; i < Number(tamanhoSenha.value); i++){
     ];
 }
 
+geraHistorico();
+
+}
+
+//Salva a senha no histórico
+function geraHistorico(){
+
+let historico;
+try {
+  historico = JSON.parse(localStorage.getItem("historico")) || [];
+} catch (e) {
+  historico = []; // se der erro, começa com array vazio
+}
+historico.push(inputResultado.value);
+
+localStorage.setItem("historico", JSON.stringify(historico));
+
+console.log(historico);
+}
+
+//Abre o Historico
+function abreHist(){
+    geradorSenhaDisplay.style.display = "none";
+    histAparece.style.display = "block";
+}
+
+//Fecha o Histórico
+function fecharHistorico(){
+    geradorSenhaDisplay.style.display = "block";
+    histAparece.style.display = "none";
+}
+
+//Apaga o Histórico
+function deletaHistorico(){
+    localStorage.removeItem("historico");
 }
 
 //Função para copiar a senha
@@ -60,6 +105,6 @@ function adicionaAoClipboard(){
     botaoCopiar.innerText = "Copiado!"
     setTimeout(()=> {
         botaoCopiar.innerText = "Copiar";
-    }, 2000); 
+    }, 1200); 
 }
 
